d2d8744ec9f0b251acf7b69df18078d2e272cf41
Kirill Balushin <balusinkirill@gmail.com>
2022-06-23 13:29:01 +0300

    products

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ var $nav = document.getElementById('nav');
 $btn.addEventListener('click', function() {
     $nav.classList.toggle('active');
 });
+
+$(document).ready(function() {
+    var body = $("body");
+    body.fadeIn(600);
+    $(document).on("click", "a:not([href^='#']):not([href^='tel']):not([href^='mailto'])", function(e) {
+        e.preventDefault();
+        $("body").fadeOut(600);
+        var self = this;
+        setTimeout(function() {
+            window.location.href = $(self).attr("href");
+        }, 600);
+    });
+});
